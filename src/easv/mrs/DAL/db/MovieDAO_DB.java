700bb9e281e4b851e7e921ae09579eea8cb11dab
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2022-11-21 12:55:03 +0100

    DB start impl2

--- src/easv/mrs/DAL/db/MovieDAO_DB.java
+++ src/easv/mrs/DAL/db/MovieDAO_DB.java
@@ -3,6 +3,7 @@ package easv.mrs.DAL.db;
 import easv.mrs.BE.Movie;
 import easv.mrs.DAL.IMovieDataAccess;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +20,12 @@ public class MovieDAO_DB implements IMovieDataAccess {
 
         ArrayList<Movie> allMovies = new ArrayList<>();
 
-        try (Connection conn = databaseConnector.getConnection())
+        try (Connection conn = databaseConnector.getConnection();
+             Statement stmt = conn.createStatement())
         {
             String sql = "SELECT * FROM Movie;";
 
-            Statement stmt = conn.createStatement();
+
             ResultSet rs = stmt.executeQuery(sql);
 
             // Loop through rows from the database result set
@@ -48,8 +50,37 @@ public class MovieDAO_DB implements IMovieDataAccess {
     }
 
     public Movie createMovie(String title, int year) throws Exception {
-        //TODO Do this
-        throw new UnsupportedOperationException();
+
+        String sql = "INSERT INTO Movie (Title,Year) VALUES (?,?);";
+
+        try (Connection conn = databaseConnector.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            // Bind parameters
+            stmt.setString(1,title);
+            stmt.setInt(2, year);
+
+            // Run the specified SQL statement
+            stmt.executeUpdate();
+
+            // Get the generated ID from the DB
+            ResultSet rs = stmt.getGeneratedKeys();
+            int id = 0;
+
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+
+            // Create movie object and send up the layers
+            Movie movie = new Movie(id, year, title);
+            return movie;
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+            throw new Exception("Could not create movie", ex);
+        }
+
     }
 
     public void updateMovie(Movie movie) throws Exception {
